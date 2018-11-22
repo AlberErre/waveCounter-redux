a79f38620a76bcde986553f0f46097825432464c
Alber Erre <arblugo@gmail.com>
2018-11-22 01:11:02 +0000

    update TextCount & .gitignore

--- src/components/TextCount.js
+++ src/components/TextCount.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
 
 const TextCount = (props) => (
 
     <Text style={styles.textDisplayed}>
       {props.counter}
     </Text>
-    
 );
 
 export default TextCount;
 
 const styles = StyleSheet.create({
-  textDisplayed: {
-    fontSize: 120,
-  }
+    textDisplayed: {
+      fontSize: 120,
+    }
 });
