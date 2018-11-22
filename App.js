1c195e499ad8310be8f277c413546e09e81270e4
Alber Erre <arblugo@gmail.com>
2018-11-22 01:43:47 +0000

    rm comments

--- App.js
+++ App.js
@@ -3,13 +3,11 @@ import Counter from './src/Counter';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-// Redux stuff
 
 import reducer from './src/reducers/reducer';
 
 const store = createStore(reducer);
 
-// end Redux stuff
 
 class App extends Component {
   constructor(props) {
@@ -18,8 +16,6 @@ class App extends Component {
 
   render() {
     return (
-    	// Provider va aqui para envolver toda la app con el global store
-    	// Para que comunique, tenemos que usar "connect" dentro de "Counter"
 		<Provider store={store}>
 	  		<Counter />
 	 	</Provider>
