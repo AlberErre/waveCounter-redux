b634f6cb7d7732f3485885fcd162e423e333734d
Alber Erre <arblugo@gmail.com>
2018-11-22 01:10:01 +0000

    add button style config

---
+++ src/components/buttons/style/buttonStyle.js
@@ -0,0 +1,19 @@
+const buttonStyle = {
+
+    reset: {
+        size: 30,
+        color: '#000000'
+    },
+
+    green: {
+        size: 100,
+        color: '#66ff66'
+    },
+
+    red: {
+        size: 100,
+        color: '#ff6666'
+    }
+};
+
+export default buttonStyle;
