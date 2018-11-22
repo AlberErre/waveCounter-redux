783fa964902a72e0af05167c7cce43e527935d1d
Alber Erre <arblugo@gmail.com>
2018-11-22 00:21:38 +0000

    clear actions object
    Actions have to be a plain JS object defining the possible actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-
 const actions = {
-
+	
 	add: {
 		type: 'INCREASE_COUNT'
 	},
-
+	
 	sub: {
 		type: 'DECREASE_COUNT'
 	},
-
+	
 	reset: {
 		type: 'RESET_COUNT'
 	}
